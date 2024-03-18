b3d7b26f9bd741cb414b43c29341d7cd2bfd3043
James Diamond <james.diamond@icloud.com>
2024-03-18 14:35:39 +0000

    fixed syntax

--- nnfs.py
+++ nnfs.py
@@ -14,7 +14,7 @@ X = [[1, 2, 3, 2.5],
 def create_data(points, classes):
     X = np.zeros((points*classes, 2))
     y = np.zeros(points*classes, dtype='uint8')
-    for class_number in range(classes);
+    for class_number in range(classes):
         ix = range(points*class_number, points*(class_number+1))
         r = np.linspace(0.0, 1, points) #radius
         t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
