e69ef97f7d10d59f994efebae55e15412150b7f1
James Diamond <james.diamond@icloud.com>
2024-03-18 14:35:10 +0000

    added sample data set function

--- nnfs.py
+++ nnfs.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import matplotlib as plt
 
 np.random.seed(0)
 
@@ -7,6 +8,19 @@ np.random.seed(0)
 X = [[1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]]
+
+#create sample data set
+
+def create_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes);
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points) #radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -20,6 +34,7 @@ class Layer_Dense:
 class Activation_ReLU:
     def forward(self, inputs):
         self.output.np.maximum(0, inputs)
+
 
 layer1 = Layer_Dense(len(X[0]),5)
 layer1.forward(X)
